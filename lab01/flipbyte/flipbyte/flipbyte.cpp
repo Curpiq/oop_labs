9529b5c978b1daa97aa807dca85244bd8035e606
Alyona Semyonova <alien.semy@gmail.com>
2020-03-16 12:16:11 +0300

    Update flipbyte.cpp

--- lab01/flipbyte/flipbyte/flipbyte.cpp
+++ lab01/flipbyte/flipbyte/flipbyte.cpp
@@ -5,15 +5,14 @@
 #include <string>
 using namespace std;
 
-int ParseArgs(int argc, char* argv[])
+bool ParseArgs(int argc, char* argv[], unsigned int& inputByte)
 {
     if (argc != 2)
     {
         cout << "Invalid arguments count\n" << "Usage: flipbyte.exe <decimal number>" << endl;
-        return -1;
+        return false;
     }
-    bool parsed = true;
-    int num;
+    unsigned int num;
     try
     {
         num = stoi(argv[1]);
@@ -21,52 +20,42 @@ int ParseArgs(int argc, char* argv[])
     catch (const invalid_argument & err)
     {
         cout << err.what() << endl;
-        parsed = false;
+        return false;
     }
     catch (const out_of_range & err)
     {
         cout << err.what() << endl;
-        parsed = false;
-    }
-
-    if (!parsed)
-    {
-        cout << "Input must be a decimal number in the byte range" << endl;
-        return -1;
+        return false;
     }
 
     if ((num < 0) || (num > 255))
     {
         cout << "Invalid argument. Range of numbers: 0 - 255" << endl;
-        return -1;
+        return false;
     }
-
-    return num;
+    inputByte = num;
+    return true;
 }
 
-unsigned int FlipByte(int num)
+unsigned int FlipByte(unsigned int number)
 {
-    unsigned int number = num;
-    unsigned int revNum;
-    for (revNum = 0; number; number >>= 1) {
+    unsigned int revNum = 0;
+    for (int i = 0; i < 8; i++) {
         revNum <<= 1;
         revNum |= number & 1;
-    }
-    if (num < 128)
-    {
-        revNum <<= 7 - (int)logb(revNum);
+        number >>= 1;
     }
     return revNum;
 }
 
 int main(int argc, char* argv[])
 {
-    int num = ParseArgs(argc, argv);
-    if (num == -1)
+    unsigned int inputByte;
+    if (!ParseArgs(argc, argv, inputByte))
     {
+        cout << "Input must be a decimal number in the byte range" << endl;
         return 1;
     }
-    unsigned int revNum = FlipByte(num);
-    cout << revNum << endl;
+    cout << FlipByte(inputByte) << endl;
     return 0;
 }

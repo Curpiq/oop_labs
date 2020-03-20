1e7c82aec8799565e4332e5d6ef588b12b3bc7b2
Alyona Semyonova <alien.semy@gmail.com>
2020-03-20 20:44:00 +0300

    Revert "исправлена программа"
    This reverts commit 77639ab9bbdde44b708f84368214b1da286bf93b.

--- lab01/compare/compare/compare.cpp
+++ lab01/compare/compare/compare.cpp
@@ -14,12 +14,6 @@ struct Args
     string fileName2;
 };
 
-struct ComparationResults
-{
-    int num;
-    bool isEqual;
-};
-
 optional<Args> ParseArgs(int argc, char* argv[])
 {
     if (argc != 3)
@@ -33,57 +27,26 @@ optional<Args> ParseArgs(int argc, char* argv[])
     return args;
 }
 
-optional<ComparationResults> CompareFiles(istream& file1, istream& file2)
+void CompareFiles(ifstream& file1, ifstream& file2, bool& isEqual, int& count)
 {
     //Сравнение содержимого файлов
-    string str1;
-    string str2;
-    ComparationResults comparationResults;
-    comparationResults.isEqual = false;
-    comparationResults.num = 1;
+    string str1 = " ";
+    string str2 = " ";
     while (!file1.eof() && !file2.eof())
     {
         getline(file1, str1);
         getline(file2, str2);
         if (str1 != str2)
         {
-            comparationResults.isEqual = false;
-            return comparationResults;
+            isEqual = false;
+            break;
         }
-        comparationResults.num++;
-        comparationResults.isEqual = true;
+        else
+        {
+            count++;
+            isEqual = true;
+        }
     }
-    return comparationResults;
-}
-
-int Comparation(optional<Args>& args)
-{
-    //Открытие файлов 
-    ifstream file1;
-    file1.open(args->fileName1);
-    ifstream file2;
-    file2.open(args->fileName2);
-    if (!file1.is_open() || !file2.is_open())
-    {
-        cout << "Failed to open " << args->fileName1 << " and/or " << args->fileName2 << " for reading" << endl;
-        return -1;
-    }
-    auto comparationResults = CompareFiles(file1, file2);
-    if (file1.bad())
-    {
-        cout << "Failed to read data from " << args->fileName1 << endl;
-        return -1;
-    }
-    if (file2.bad())
-    {
-        cout << "Failed to read data from " << args->fileName1 << endl;
-        return -1;
-    }
-    if (file1.eof() && file2.eof() && comparationResults->isEqual == true)
-    {
-        return 0;
-    }
-    return comparationResults->num;
 }
 
 int main(int argc, char* argv[])
@@ -94,16 +57,35 @@ int main(int argc, char* argv[])
     {
         return 1;
     }
-    int result = Comparation(args);
-    if (result == -1)
+
+    //Открытие файлов 
+    ifstream file1;
+    file1.open(args->fileName1);
+    ifstream file2;
+    file2.open(args->fileName2);
+    if (!file1.is_open() || !file2.is_open())
     {
+        cout << "Failed to open " << args->fileName1 << " and/or " << args->fileName2 << " for reading" << endl;
         return 1;
     }
-    if (result == 0)
+    bool isEqual = false;
+    int count = 1;
+    CompareFiles(file1, file2, isEqual, count);
+    if (file1.bad())
+    {
+        cout << "Failed to read data from " << args->fileName1 << endl;
+        return 1;
+    }
+    if (file2.bad())
+    {
+        cout << "Failed to read data from " << args->fileName1 << endl;
+        return 1;
+    }
+    if (file1.eof() && file2.eof() && isEqual == true)
     {
         cout << "Files are equal" << endl;
         return 0;
     }
-    cout << "Files are different. Line number is " << result << endl;
+    cout << "Files are different. Line number is " << count << endl;
     return 1;
 }

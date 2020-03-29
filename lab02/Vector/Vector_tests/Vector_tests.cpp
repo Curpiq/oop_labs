beb5717fdfbf5dde78e8fcb0f2e2486843b7c18d
Alyona Semyonova <alien.semy@gmail.com>
2020-03-30 00:35:18 +0300

    tests for task1

---
+++ lab02/Vector/Vector_tests/Vector_tests.cpp
@@ -0,0 +1,25 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+#include "../Vector/VectorProcessor.h"
+
+using namespace std;
+
+bool ArraysAreEqual(vector<double> const& x, vector<double> const& y)
+{
+	return x == y;
+}
+
+TEST_CASE("Make empty vector from empty vector")
+{
+	vector<double> emptyVector;
+	ProcessVector(emptyVector);
+	CHECK(emptyVector.empty());
+}
+
+TEST_CASE("Make a vector, each element of which is multiplied by maximum element and divided by minimum element of the original vector")
+{
+	vector<double> numbers = { 4, 3, 2, 10 };
+	ProcessVector(numbers);
+	vector<double> expected = { 20, 15, 10, 50 };
+	CHECK(ArraysAreEqual(numbers, expected));
+}
